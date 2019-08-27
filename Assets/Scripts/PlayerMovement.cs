16a4029f73b5d553da6e93c5f1249f91c1236890
KoreAlonso <andreastur96@gmail.com>
2019-08-27 21:38:40 +0200

    Version : 0.0.1 - Primer Controlador

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    CharacterController samurai;
+    float velocity = 4;
+    float dashVelocity = 25;
+    float rotateVelocity = 5;
+ 
+
+    //Variable para acceder a la Clase GetInputs
+    GetInputs inputs;
+
+    //Guardan el movimiento (velocidad * direccion)
+    Vector3 samuraiMovement;
+    Vector3 samuraiDash;
+
+    //Variables método Timer();
+    float currentDashTime;
+    float maxDashTime = 0.3f;
+    
+    void Start()
+    {
+        //Obtengo instancias
+        samurai = GetComponent<CharacterController>();
+        inputs = FindObjectOfType<GetInputs>();
+    }
+     void Update()
+    {
+        rotate();
+        dash();
+        movementDirection();
+    }
+
+    //método que recibe un Vector3. Se encarga de aplicar un SimpleMove (dependiente de la anterior variable) al CharacterController 
+     void movement(Vector3  inputDirection)
+    {
+        samurai.SimpleMove(inputDirection);
+    }
+
+    //encargado del movimiento continuo
+    void movementDirection()
+    { 
+        if (inputs.getHorizontal() != 0 || inputs.getVertical() != 0)
+        {  
+            //se asignan comandos y dirección. Se cambian los ejes globales por defecto, a los ejes locales del CharacterController
+          samuraiMovement = new Vector3(inputs.getHorizontal(), 0, inputs.getVertical());
+          samuraiMovement = transform.TransformDirection(samuraiMovement);
+
+            //se llama al método movement (Vector3 * float)
+          movement(samuraiMovement * velocity);  
+        }
+    }
+
+    //encargado de rotar al Pj.
+    void rotate()
+    {
+         transform.Rotate(0, Input.GetAxis("Mouse X") * rotateVelocity, 0);
+    }
+
+    //encargado del movimiento Dash
+   void dash()
+    {   
+        if (Input.GetKey(KeyCode.Space))
+            {
+              if(timerDash() < maxDashTime)
+              { 
+                timerDash();
+
+                samuraiDash = new Vector3(inputs.getHorizontal() , 0, inputs.getVertical() );
+                samuraiDash = transform.TransformDirection(samuraiDash);
+
+                movement(samuraiDash * dashVelocity);
+              }
+            else
+            {   //equivale a 0
+                samuraiDash = Vector3.zero;
+            }
+        }
+       
+   }
+    //encargado de ejecutar y devolver el tiempo actual. Primero comprueba si este es mayor al tiempo maximo. Si es asi, lo devuelve a 0 
+     float  timerDash()
+     {      
+        if (Input.GetKeyDown(KeyCode.Space) && currentDashTime >= maxDashTime)
+        {
+            currentDashTime = 0.0f;
+        }
+
+        currentDashTime = currentDashTime + 1 * Time.deltaTime;
+
+        return currentDashTime;
+     }
+    
+    
+    
+}
