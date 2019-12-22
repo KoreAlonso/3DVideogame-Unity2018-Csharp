406530cdd13c14feeabe3cb943fbc13ac3139dd6
KoreAlonso <andreastur96@gmail.com>
2019-12-22 16:28:30 +0100

    version 2.1.1 - Mecanicas de juego finalizadas a falta de UI y cambios de escena

---
+++ Assets/Scripts/Tools/RandomItem.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomItem : MonoBehaviour
+{
+    public static RandomItem sharedInstance;
+
+    public GameObject item;
+    public GameObject instanceItem;
+    Vector3 positionItem;
+    Quaternion rotationItem;
+    public float itemShotVelocity = 0.8f;
+    public float itemEnergyIncrease = 2;
+
+    LifeBarPlayer lifeBarPlayer;
+
+    public float minTime = 0, currentTime, maxTime;
+
+    void Start()
+    {
+        sharedInstance = this;
+        item = GameObject.FindGameObjectWithTag("Item");
+        positionItem = new Vector3(Random.Range(0, 100), 1, Random.Range(0, 100));
+        rotationItem = new Quaternion(0, 0, 25, 0);
+
+        lifeBarPlayer = FindObjectOfType<LifeBarPlayer>();
+
+        currentTime = minTime;
+        maxTime = Random.Range(50, 120);
+    }
+
+    void Update()
+    {
+        spawnItem();
+    }
+
+    void spawnItem()
+    {
+        if (lifeBarPlayer.currentLife < 0.25 * lifeBarPlayer.maxLife && currentTime == minTime)
+        {
+            currentTime += Time.deltaTime;
+            if (currentTime >= maxTime && instanceItem == null)
+            {
+                instanceItem = Instantiate(item, positionItem, rotationItem, item.transform);
+            }
+        }
+    }
+
+}
